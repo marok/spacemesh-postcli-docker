0e19ee539bf572576f49f503c594f7f8b3e592d9
Mariusz Okroj <okrojmariusz@gmail.com>
2023-08-27 12:18:10 +0200

    added possibility to add node id and post file range via env

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
-ID_ARG=
+if [ ! -z "$NODE_ID" ]
+then
+	echo "Node ID provided via NODE_ID env: $NODE_ID"
+	ID_ARG="-id $NODE_ID"
+else
+	ID_ARG=
+fi
 
-if [ -f "$DATA_DIR/key.bin" ]; then
+if [ -z "$ID_ARG" ] && [ -f "$DATA_DIR/key.bin" ]
+then
+	echo "Node ID read from key.bin: $(cat $DATA_DIR/key.bin | tail -c 64)"
 	ID_ARG="-id $(cat $DATA_DIR/key.bin | tail -c 64)"
+fi
+
+GEN_POST_RANGE_ARG=
+if [ ! -z "$FROM_FILE" ] && [ ! -z "$TO_FILE" ]; then
+	echo "Generating only subset of files:"
+	echo "	from: $FROM_FILE"
+	echo "	to:   $TO_FILE"
+	GEN_POST_RANGE_ARG="-fromFile $FROM_FILE -toFile $TO_FILE"
 fi
 
 set -x
@@ -18,4 +34,5 @@ clinfo -l
 	-numUnits=$NUM_UNITS \
  	-provider=$PROVIDER \
 	-maxFileSize=2147483648 \
+	$GEN_POST_RANGE_ARG \
 	$ID_ARG || sleep inf
